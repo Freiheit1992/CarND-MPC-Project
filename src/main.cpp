c85e720fe06776b9f7b304e100997e85d02a4035
Freiheit1992 <40829723+Freiheit1992@users.noreply.github.com>
2020-02-05 18:38:55 +0800

    Add files via upload
    50mph ok

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,8 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
-int main() {
+int main()
+{
   uWS::Hub h;
 
   // MPC is initialized here!
@@ -34,12 +35,15 @@ int main() {
     // The 2 signifies a websocket event
     string sdata = string(data).substr(0, length);
     std::cout << sdata << std::endl;
-    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2')
+    {
       string s = hasData(sdata);
-      if (s != "") {
+      if (s != "")
+      {
         auto j = json::parse(s);
         string event = j[0].get<string>();
-        if (event == "telemetry") {
+        if (event == "telemetry")
+        {
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
@@ -47,24 +51,68 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double delta= j[1]["steering_angle"];
+          // delta *= deg2rad(25);
+          double a = j[1]["throttle"];
+          double delay = 0.05;
+          px += v*cos(psi) *delay;
+          py += v*sin(psi) *delay;
+          psi -= v/ 2.67 * delta * delay;
+          for (int i = 0; i < ptsx.size(); ++i)
+          {
+            double x = (ptsx[i] - px) * std::cos(psi) + (ptsy[i] - py) * std::sin(psi);
+            ptsy[i] = -(ptsx[i] - px) * std::sin(psi) + (ptsy[i] - py) * std::cos(psi);
+            ptsx[i] = x;
+          }
 
           /**
            * TODO: Calculate steering angle and throttle using MPC.
            * Both are in between [-1, 1].
            */
-          double steer_value;
-          double throttle_value;
+          px = 0;
+          py = 0;
+          psi = 0;
+          VectorXd ptsx_vxd = VectorXd::Map(&ptsx[0], ptsx.size());
+          VectorXd ptsy_vxd = VectorXd::Map(&ptsy[0], ptsy.size());
+          auto coeffs = polyfit(ptsx_vxd, ptsy_vxd, 3);
+          double cte = polyeval(coeffs, px) - py;
+          double ref_psi_tan = 0;
+          for (int i = 1; i < coeffs.size(); i++)
+            ref_psi_tan += i * coeffs[i] * std::pow(px, i - 1);
+          double ref_psi = atan(ref_psi_tan);
+          double epsi = ref_psi - psi;
+          std::cout << "epsi: " << epsi << ", cte: " << cte << std::endl;
+
+          // double x_delay =0+ ( v * cos(0) * delay );
+          // double y_delay = 0+( v * sin(0) * delay );
+          // double psi_delay =0 - ( v * delta * delay /2.67 );
+          // double v_delay = v + a * delay;
+          // double cte_delay = cte + ( v * sin(epsi) * delay );
+          // double epsi_delay = epsi - ( v * delta * delay /2.67 );
+          VectorXd state(6);
+          state << px, py, psi, v, cte, epsi;
+
+          auto vars = mpc.Solve(state, coeffs);
+          int var_l = vars.size();
+          double steer_value = -vars[var_l - 2] / deg2rad(25);
+          double throttle_value = vars[var_l - 1];
 
           json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the 
-          //   steering value back. Otherwise the values will be in between 
+          // NOTE: Remember to divide by deg2rad(25) before you send the
+          //   steering value back. Otherwise the values will be in between
           //   [-deg2rad(25), deg2rad(25] instead of [-1, 1].
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          // Display the MPC predicted trajectory 
+          // Display the MPC predicted trajectory
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
+          int N = (var_l - 2) / 2;
+          for (int i = 0; i < N; ++i)
+          {
+            mpc_x_vals.push_back(vars[i]);
+            mpc_y_vals.push_back(vars[i+N]);
+          }
 
           /**
            * TODO: add (x,y) points to list here, points are in reference to 
@@ -84,10 +132,22 @@ int main() {
            *   the vehicle's coordinate system the points in the simulator are 
            *   connected by a Yellow line
            */
-
+          // for (int i = 0; i < ptsx.size(); ++i)
+          // {
+          //   // psi = 0;
+          //   next_x_vals.push_back((ptsx[i]-px) * std::cos(psi) + (ptsy[i]-py)  * std::sin(psi));
+          //   next_y_vals.push_back(-(ptsx[i]-px)  * std::sin(psi) + (ptsy[i]-py) * std::cos(psi));
+          //   // std::cout << "x, y: " << -ptsx[i] + px * std::cos(psi) + py * std::sin(psi) << ", " << -ptsy[i] - px * std::sin(psi) + py * std::cos(psi);
+          // }
+          // next_x_vals = ptsx;
+          // next_y_vals = ptsy;
+          for (int i=1; i< 20; ++i){
+            next_x_vals.push_back(3*i);
+            next_y_vals.push_back(polyeval(coeffs, 3*i));
+          }
+          
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
-
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
@@ -101,13 +161,15 @@ int main() {
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
           std::this_thread::sleep_for(std::chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
-      } else {
+        } // end "telemetry" if
+      }
+      else
+      {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
-    }  // end websocket if
+    } // end websocket if
   }); // end h.onMessage
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
@@ -121,12 +183,15 @@ int main() {
   });
 
   int port = 4567;
-  if (h.listen(port)) {
+  if (h.listen(port))
+  {
     std::cout << "Listening to port " << port << std::endl;
-  } else {
+  }
+  else
+  {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
